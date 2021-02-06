7adc8432d6327ac24025d535a114ac1f4504b910
Fenzaj <gambler365@gmail.com>
2021-02-06 23:05:30 +0100

    Restored the file from second commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,5 @@
-//This program  will print simple message 
 public class Hello {
   public static void main(String[] args){
     System.out.print("Hello World\n");
-    //Print "Welcome to Git" in the terminal windows. 
-    System.out.print("Welcome to Git!\n");
   }
 }
