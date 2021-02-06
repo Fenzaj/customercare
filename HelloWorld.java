23b3573ed5febc6f338884043b303e0b8f5eb604
Fenzaj <gambler365@gmail.com>
2021-02-06 20:51:32 +0100

    Added a comment line for a new print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@
 public class Hello {
   public static void main(String[] args){
     System.out.print("Hello World\n");
+    //Print "Welcome to Git" in the terminal windows. 
     System.out.print("Welcome to Git!\n");
   }
 }
